ea73f3afc5d13da9ecc2c2a37e1e0d6f382a2ee6
ScarredCG <Sutterlinchristian@gmail.com>
2017-04-17 08:42:41 -0700

    Update Folder.cpp

--- P3/P3/Folder.cpp
+++ P3/P3/Folder.cpp
@@ -1,80 +1,74 @@
+#include <iterator>
 #include "Folder.h"
 
 Folder::Folder(std::string name) : _name(name)
 {
-
 }
+
+Folder::Folder(){}
 
 Folder::~Folder()
 {
-
 }
 
 std::vector<std::string> Folder::ListFiles()
 {
-
 	std::vector<std::string> list;
-
 	for (auto file : _files) {
-
 		list.push_back(file->getName());
-
 	}
-
 	return list;
-
 }
 
 std::string Folder::ListContents()
 {
-
 	std::string output = "\n";
-
 	for (auto folder : _folders) {
-
 		output += "\\" + folder->getName() + "\n";
-
 	}
-
 	for (auto file : _files) {
-
-		output += file->getName() + "\n";
-
+		output += file->getName() + "\n";		
 	}
-
 	return output;
-
 }
 
 void Folder::AddFile(std::shared_ptr<File> newFile)
 {
-
 	_files.push_back(newFile);
-
 }
 
 void Folder::AddFolder(std::shared_ptr<Folder> newFolder)
 {
-
 	_folders.push_back(newFolder);
-
 }
 
 std::shared_ptr<Folder> Folder::FindFolder(std::string name)
 {
-
-	for (auto folder : _folders)
-
+	for(auto folder : _folders)
 	{
-
 		if (folder->getName() == name) {
-
 			return folder;
-
 		}
-
 	}
-
 	return nullptr;
+}
 
+std::shared_ptr<File> Folder::FindFile(std::string _name)
+{
+	for (auto file : _files)
+	{
+		if (file->getName() == _name)
+		{
+			return file;
+		}
+		else
+		{
+			for (auto folder : _folders)
+			{
+				folder->FindFile(_name);
+			}
+		}
+	}
+	
+	return nullptr;
 }
